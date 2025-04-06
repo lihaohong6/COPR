053364122ab2b17a54681417941f1ad7d8c3dfa0
Peter Li <lihaohong6@gmail.com>
2025-04-06 01:51:49 -0400

    autoupdate forgejo-cli and codeberg-cli

--- autoupdate/config.py
+++ autoupdate/config.py
@@ -11,4 +11,6 @@ all_projects = [
     CoprProject("scc", 376740),
     CoprProject("bazel", 15227, test_build_locally=False),
     CoprProject("bazel7", 15227, copr_repo="bazel", version_filter=lambda v: v.startswith("7"), test_build_locally=False),
+    CoprProject("codeberg-cli", 377554),
+    CoprProject("forgejo-cli", 377539)
 ]
